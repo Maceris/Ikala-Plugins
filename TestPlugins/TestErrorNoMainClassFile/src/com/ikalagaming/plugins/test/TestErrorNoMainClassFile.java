471aa2efef587aa06c846b3180a98ba4e8f4bc7a
maceris <chesburks@gmail.com>
2021-08-05 20:19:11 -0400

    Add test for incorrectly configured main class file

---
+++ TestPlugins/TestErrorNoMainClassFile/src/com/ikalagaming/plugins/test/TestErrorNoMainClassFile.java
@@ -0,0 +1,11 @@
+package com.ikalagaming.plugins.test;
+
+import com.ikalagaming.plugins.Plugin;
+
+/**
+ * This should not be pointed to by the plugin yaml file.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class TestErrorNoMainClassFile extends Plugin {}
