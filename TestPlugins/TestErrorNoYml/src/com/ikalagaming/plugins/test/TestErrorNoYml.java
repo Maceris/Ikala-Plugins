a89a16b5ac5ed647990790581cada66444170763
maceris <chesburks@gmail.com>
2021-08-06 16:59:27 -0400

    Add missing yml error scenario

---
+++ TestPlugins/TestErrorNoYml/src/com/ikalagaming/plugins/test/TestErrorNoYml.java
@@ -0,0 +1,11 @@
+package com.ikalagaming.plugins.test;
+
+import com.ikalagaming.plugins.Plugin;
+
+/**
+ * A plugin that is missing the plugin.yml file.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class TestErrorNoYml extends Plugin {}
