90e3ecd8305041975dc6cea1ae0e9289e2e48b04
maceris <chesburks@gmail.com>
2021-08-05 20:16:33 -0400

    Add complex dependency chain

---
+++ TestPlugins/TestComplexChainC/src/com/ikalagaming/plugins/test/TestComplexChainC.java
@@ -0,0 +1,11 @@
+package com.ikalagaming.plugins.test;
+
+import com.ikalagaming.plugins.Plugin;
+
+/**
+ * A plugin that has a more complex dependency chain.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class TestComplexChainC extends Plugin {}
