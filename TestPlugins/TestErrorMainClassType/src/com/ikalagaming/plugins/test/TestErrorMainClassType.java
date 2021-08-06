3e6d5bd339557045f013fdd82d2c316ba0eaec8f
maceris <chesburks@gmail.com>
2021-08-06 16:59:15 -0400

    Add main class type error scenario

---
+++ TestPlugins/TestErrorMainClassType/src/com/ikalagaming/plugins/test/TestErrorMainClassType.java
@@ -0,0 +1,11 @@
+package com.ikalagaming.plugins.test;
+
+import java.beans.Beans;
+
+/**
+ * Not a plugin class.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class TestErrorMainClassType extends Beans {}
