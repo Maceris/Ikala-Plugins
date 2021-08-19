d0bdc0f9196417c2ee38446ae0f981d47fa51481
maceris <chesburks@gmail.com>
2021-08-18 23:54:54 -0400

    Initial setup for Random plugin

---
+++ Random/README.md
@@ -0,0 +1,4 @@
+# Random
+
+This plugin provides utilities for randomness. Random number generation,
+selecting random things from a list, noise, etc.
---
+++ Random/src/com/ikalagaming/random/RandomPlugin.java
@@ -0,0 +1,11 @@
+package com.ikalagaming.random;
+
+import com.ikalagaming.plugins.Plugin;
+
+/**
+ * An example plugin to demonstrate how plugins are set up.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class RandomPlugin extends Plugin {}
