94bf8264a36e034360ce185cf2bcf8e6b5d3ccc8
maceris <chesburks@gmail.com>
2021-09-02 18:12:46 -0400

    Add some more tests for getters

--- EntityComponentSystem/test/com/ikalagaming/ecs/TestECSManager.java
+++ EntityComponentSystem/test/com/ikalagaming/ecs/TestECSManager.java
@@ -1,8 +1,11 @@
 package com.ikalagaming.ecs;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,6 +16,14 @@ import java.util.UUID;
  *
  */
 public class TestECSManager {
+
+	/**
+	 * Setup before each test.
+	 */
+	@Before
+	public void beforeTest() {
+		ECSManager.clear();
+	}
 
 	/**
 	 * Test that components can be stored and retrieved.
@@ -111,4 +122,133 @@ public class TestECSManager {
 		retA1 = ECSManager.getComponent(entity3, TestA.class);
 		Assert.assertFalse(retA1.isPresent());
 	}
+
+	/**
+	 * Tests the {@link ECSManager#getAllComponents(Class)} method.
+	 */
+	@Test
+	public void testGetAllComponents() {
+		// Empty case
+		List<TestA> aComponents = ECSManager.getAllComponents(TestA.class);
+		Assert.assertNotNull(aComponents);
+		Assert.assertTrue(aComponents.isEmpty());
+
+		// Normal flow
+		UUID entity1 = ECSManager.createEntity();
+		UUID entity2 = ECSManager.createEntity();
+		UUID entity3 = ECSManager.createEntity();
+
+		TestA a1 = new TestA();
+		TestA a2 = new TestA();
+		TestB b1 = new TestB();
+		TestB b3 = new TestB();
+
+		ECSManager.addComponent(entity1, a1);
+		ECSManager.addComponent(entity1, b1);
+		ECSManager.addComponent(entity2, a2);
+		ECSManager.addComponent(entity3, b3);
+
+		aComponents = ECSManager.getAllComponents(TestA.class);
+		List<TestB> bComponents = ECSManager.getAllComponents(TestB.class);
+
+		Assert.assertNotNull(aComponents);
+		Assert.assertEquals(2, aComponents.size());
+
+		Assert.assertNotNull(bComponents);
+		Assert.assertEquals(2, bComponents.size());
+
+		Assert.assertTrue(aComponents.contains(a1));
+		Assert.assertTrue(aComponents.contains(a2));
+
+		Assert.assertTrue(bComponents.contains(b1));
+		Assert.assertTrue(bComponents.contains(b3));
+
+		// See if removing components works as expected
+		ECSManager.removeComponent(entity1, TestA.class);
+		aComponents = ECSManager.getAllComponents(TestA.class);
+		Assert.assertNotNull(aComponents);
+		Assert.assertEquals(1, aComponents.size());
+		Assert.assertTrue(aComponents.contains(a2));
+	}
+
+	/**
+	 * Tests the {@link ECSManager#getAllEntities()} method.
+	 */
+	@Test
+	public void testGetAllEntities() {
+		List<UUID> entities = ECSManager.getAllEntities();
+		Assert.assertNotNull(entities);
+		Assert.assertTrue(entities.isEmpty());
+
+		List<UUID> ids = new ArrayList<>();
+		for (int i = 0; i < 5; ++i) {
+			ids.add(ECSManager.createEntity());
+		}
+
+		entities = ECSManager.getAllEntities();
+		Assert.assertNotNull(entities);
+		Assert.assertEquals(ids.size(), entities.size());
+		entities.forEach(id -> Assert.assertTrue(ids.contains(id)));
+
+		UUID removed = ids.remove(0);
+		ECSManager.destroyEntity(removed);
+
+		entities = ECSManager.getAllEntities();
+		Assert.assertNotNull(entities);
+		Assert.assertEquals(ids.size(), entities.size());
+		entities.forEach(id -> Assert.assertTrue(ids.contains(id)));
+		Assert.assertFalse(entities.contains(removed));
+	}
+
+	/**
+	 * Tests the {@link ECSManager#getAllEntitiesWithComponent(Class...)}
+	 * method.
+	 */
+	@Test
+	public void testGetAllEntitiesWithComponent() {
+		List<UUID> blank = ECSManager.getAllEntitiesWithComponent(TestA.class);
+		Assert.assertNotNull(blank);
+		Assert.assertTrue(blank.isEmpty());
+
+		UUID hasA = ECSManager.createEntity();
+		UUID hasBoth = ECSManager.createEntity();
+		UUID hasB = ECSManager.createEntity();
+		UUID hasNone = ECSManager.createEntity();
+
+		TestA a1 = new TestA();
+		TestA a2 = new TestA();
+		TestB b1 = new TestB();
+		TestB b2 = new TestB();
+
+		ECSManager.addComponent(hasA, a1);
+		ECSManager.addComponent(hasBoth, a2);
+		ECSManager.addComponent(hasBoth, b1);
+		ECSManager.addComponent(hasB, b2);
+
+		List<UUID> withA = ECSManager.getAllEntitiesWithComponent(TestA.class);
+		List<UUID> withB = ECSManager.getAllEntitiesWithComponent(TestB.class);
+		List<UUID> withAB =
+			ECSManager.getAllEntitiesWithComponent(TestA.class, TestB.class);
+
+		Assert.assertNotNull(withA);
+		Assert.assertNotNull(withB);
+		Assert.assertNotNull(withAB);
+
+		Assert.assertEquals(2, withA.size());
+		Assert.assertEquals(2, withB.size());
+		Assert.assertEquals(1, withAB.size());
+
+		Assert.assertTrue(withA.contains(hasA));
+		Assert.assertTrue(withA.contains(hasBoth));
+
+		Assert.assertTrue(withB.contains(hasB));
+		Assert.assertTrue(withB.contains(hasBoth));
+
+		Assert.assertTrue(withAB.contains(hasBoth));
+
+		Assert.assertFalse(withA.contains(hasNone));
+		Assert.assertFalse(withB.contains(hasNone));
+		Assert.assertFalse(withAB.contains(hasNone));
+	}
+
 }
