4da7d18842614fbbbbebabf92a77e22bea5c537d
David Burks <chesburks@gmail.com>
2022-05-05 20:24:24 -0400

    [IR-6] Add prefixes for item and weapon types

--- Ikala-Item/src/main/java/com/ikalagaming/item/ItemType.java
+++ Ikala-Item/src/main/java/com/ikalagaming/item/ItemType.java
@@ -1,43 +1,58 @@
 package com.ikalagaming.item;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * The category of item.
  * 
  * @author Ches Burks
  *
  */
+@Getter
+@AllArgsConstructor
 public enum ItemType {
 	/**
 	 * Any sort of weapon or shield, held in the hands.
 	 */
-	WEAPON,
+	WEAPON("weapon_"),
 	/**
 	 * Any sort of armor, worn on the body.
 	 */
-	ARMOR,
+	ARMOR("armor_"),
 	/**
 	 * Any sort of accessory that does not provide protection in the sense that
 	 * armor does, worn on the body.
 	 */
-	ACCESSORY,
+	ACCESSORY("accessory_"),
 	/**
 	 * An item that sockets into another one, to augment its stats.
 	 */
-	COMPONENT,
+	COMPONENT("component_"),
 	/**
 	 * An item that is consumed when used, like food or potions.
 	 */
-	CONSUMABLE,
+	CONSUMABLE("consumable_"),
 	/**
 	 * Material that is used for crafting or which has some use.
 	 */
-	MATERIAL,
+	MATERIAL("material_"),
 	/**
 	 * Has no use at all other than being sold for currency.
 	 */
-	JUNK,
+	JUNK("junk_"),
 	/**
 	 * Items that are required for or related to quests.
 	 */
-	QUEST;
+	QUEST("quest_");
+
+	/**
+	 * The prefix that is used in the IDs for items of this type in the
+	 * database.
+	 * 
+	 * @return The prefix that appears before items of this type in the
+	 *         database.
+	 */
+	private final String prefix;
+
 }
--- Ikala-Item/src/main/java/com/ikalagaming/item/WeaponType.java
+++ Ikala-Item/src/main/java/com/ikalagaming/item/WeaponType.java
@@ -1,44 +1,58 @@
 package com.ikalagaming.item;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * What kind of weapon an item is.
  * 
  * @author Ches Burks
  *
  */
+@Getter
+@AllArgsConstructor
 public enum WeaponType {
 	/**
 	 * Melee weapons that are held in one hand, like swords, axes, or maces.
 	 */
-	ONE_HANDED_MELEE,
+	ONE_HANDED_MELEE("one_handed_melee_"),
 	/**
 	 * Melee weapons that take two hands to hold, like great swords, great axes,
 	 * mauls, or scythes.
 	 */
-	TWO_HANDED_MELEE,
+	TWO_HANDED_MELEE("two_handed_melee_"),
 	/**
 	 * Ranged weapons that are held in one hand, like pistols.
 	 */
-	ONE_HANDED_RANGED,
+	ONE_HANDED_RANGED("one_handed_ranged_"),
 	/**
 	 * Ranged weapons that take two hands to hold, like rifles or crossbows.
 	 */
-	TWO_HANDED_RANGED,
+	TWO_HANDED_RANGED("two_handed_ranged_"),
 	/**
 	 * Magic weapons that are held in one hand, like scepters or wands.
 	 */
-	ONE_HANDED_MAGIC,
+	ONE_HANDED_MAGIC("one_handed_magic_"),
 	/**
 	 * Magic weapons that take two hands to hold, like staffs.
 	 */
-	TWO_HANDED_MAGIC,
+	TWO_HANDED_MAGIC("two_handed_magic_"),
 	/**
 	 * Items that are held in the off-hand that don't fit into the other
 	 * one-handed categories, and generally provide buffs like tomes or orbs.
 	 */
-	OFF_HAND,
+	OFF_HAND("off_hand_"),
 	/**
 	 * Shields.
 	 */
-	SHIELD;
+	SHIELD("shield_");
+
+	/**
+	 * The prefix that is used in the IDs for weapons of each type in the
+	 * database, after the item type prefix.
+	 * 
+	 * @return The prefix that appears before weapons of each type, after the
+	 *         item type prefix, in the database.
+	 */
+	private final String prefix;
 }
