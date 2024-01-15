6f116b7466e174495cbb40c932951d9431581f50
David Burks <chesburks@gmail.com>
2024-01-14 19:29:02 -0500

    Add reward related classes

---
+++ Factory-Core/src/main/java/com/ikalagaming/factory/crafting/Recipe.java
@@ -0,0 +1,11 @@
+package com.ikalagaming.factory.crafting;
+
+/**
+ * Represents a crafting recipe.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class Recipe {
+
+}
---
+++ Factory-Core/src/main/java/com/ikalagaming/factory/item/Item.java
@@ -0,0 +1,11 @@
+package com.ikalagaming.factory.item;
+
+/**
+ * Represents an item in the game.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class Item {
+
+}
