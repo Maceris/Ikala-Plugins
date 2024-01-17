24a013e1f353f158f9b850a58fa67d43a3aa777a
David Burks <chesburks@gmail.com>
2024-01-16 22:32:49 -0500

    Add gas and liquid

---
+++ Factory-Core/src/main/java/com/ikalagaming/factory/item/Gas.java
@@ -0,0 +1,15 @@
+package com.ikalagaming.factory.item;
+
+/**
+ * The properties or definition of a gas in machines, containers, and recipes.
+ * 
+ * Not something that is physically present on it's own in the world. Blocks
+ * like pipes or tanks may visibly contain gases, and there are blocks that look
+ * or act like gases but those are blocks and this represents the definition.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class Gas {
+
+}
---
+++ Factory-Core/src/main/java/com/ikalagaming/factory/item/Liquid.java
@@ -0,0 +1,17 @@
+package com.ikalagaming.factory.item;
+
+/**
+ * The properties or definition of a liquid in machines, containers, and
+ * recipes.
+ * 
+ * Not something that is physically present on it's own in the world. Blocks
+ * like pipes or tanks may visibly contain fluids, and there are blocks that
+ * look and act like liquids but those are blocks and this represents the
+ * definition.
+ * 
+ * @author Ches Burks
+ *
+ */
+public class Liquid {
+
+}
