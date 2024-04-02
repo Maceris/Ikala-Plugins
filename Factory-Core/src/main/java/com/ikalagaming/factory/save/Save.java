170b663c7503abbadbcf2d8258ccd7ceb55e4676
David Burks <chesburks@gmail.com>
2024-04-01 20:53:34 -0400

    Start on a save definition

---
+++ Factory-Core/src/main/java/com/ikalagaming/factory/save/Save.java
@@ -0,0 +1,9 @@
+package com.ikalagaming.factory.save;
+
+/**
+ * The equivalent of a save file. Single player can have several saves, each might contain multiple worlds.
+ * This represents the whole game and save folder, including what worlds exist, players, registries, anything we
+ * want to track for a session and/or store on disk per save.
+ */
+public class Save {
+}
