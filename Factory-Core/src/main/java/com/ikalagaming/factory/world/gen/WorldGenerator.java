f528de410af6dbca67f763cbbe5b0b5cf1beb05d
David Burks <chesburks@gmail.com>
2024-04-15 19:36:45 -0400

    Add world height mapping function

--- Factory-Core/src/main/java/com/ikalagaming/factory/world/gen/WorldGenerator.java
+++ Factory-Core/src/main/java/com/ikalagaming/factory/world/gen/WorldGenerator.java
@@ -16,8 +16,35 @@ import java.util.Map;
  */
 public class WorldGenerator {
 
+    /**
+     * A scale parameter used for calculating the block height that the world height noise parameter
+     * value corresponds to. This value is chosen so that the total range of height values generated
+     * spans half the possible depth values. This way there is always room to dig under and build on
+     * top of the surface.
+     */
+    private static final int WORLD_HEIGHT_SCALE = World.WORLD_HEIGHT_TOTAL / 4;
+
     private static Map<String, BiomeParameters> biomes = new HashMap<>();
     private static Map<String, Byte> biomeEncoding = new HashMap<>();
+
+    /**
+     * Convert from the height parameter to the actual world height in blocks. This serves as the
+     * starting point for the world height, but may be modified by various other world generation.
+     *
+     * <p>The height values generated span half the possible world depth values so there is always
+     * room to dig under and build on top of the surface.
+     *
+     * <p>The function is cubic, so there is a relatively flat region in the middle of the range,
+     * but steep extremes.
+     *
+     * @param height The height parameter, a float in the range [0, 1] inclusive.
+     * @return The corresponding height value, in the range [-256, 256].
+     */
+    private static int calculateHeight(final float height) {
+        final float base = (height * 2 - 1);
+        final float result = WORLD_HEIGHT_SCALE * base * base * base;
+        return Math.round(result);
+    }
 
     private static int compareBiomes(
             BiomeParameters biome1, BiomeParameters biome2, ParameterPack target) {
@@ -59,5 +86,10 @@ public class WorldGenerator {
                 .map(Map.Entry::getKey)
                 .findFirst()
                 .orElse(null);
+    }
+
+    /** Private constructor so that this class is not instantiated. */
+    private WorldGenerator() {
+        throw new UnsupportedOperationException("This utility class should not be instantiated");
     }
 }
