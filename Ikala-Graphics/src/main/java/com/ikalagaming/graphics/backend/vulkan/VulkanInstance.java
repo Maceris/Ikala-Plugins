321554d0f3b691cb779d983887c26e63a412fa92
David Burks <chesburks@gmail.com>
2024-05-21 21:56:59 -0400

    Fill out application info

--- Ikala-Graphics/src/main/java/com/ikalagaming/graphics/backend/vulkan/VulkanInstance.java
+++ Ikala-Graphics/src/main/java/com/ikalagaming/graphics/backend/vulkan/VulkanInstance.java
@@ -9,11 +9,15 @@ import com.ikalagaming.graphics.scene.Scene;
 
 import lombok.NonNull;
 import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.VK;
 import org.lwjgl.vulkan.VkApplicationInfo;
 import org.lwjgl.vulkan.VkLayerProperties;
 
+import java.nio.ByteBuffer;
+
 public class VulkanInstance implements Instance {
 
+    private static final long NULL = 0L;
     private static final String[] REQUIRED_LAYERS = {};
 
     private static final String[] VALIDATION_LAYERS = {"VK_LAYER_KHRONOS_validation"};
@@ -25,7 +29,18 @@ public class VulkanInstance implements Instance {
 
     private void createVulkanInstance() {
         try (MemoryStack stack = stackPush()) {
-            VkApplicationInfo applicationInfo = VkApplicationInfo.malloc(stack);
+
+            ByteBuffer appName = stack.UTF8("Ikala-Graphics");
+            ByteBuffer engineName = stack.UTF8("Ikala Engine");
+
+            VkApplicationInfo applicationInfo = VkApplicationInfo.malloc(stack)
+                    .sType$Default()
+                    .pNext(NULL)
+                    .pApplicationName(appName)
+                    .applicationVersion(1)
+                    .pEngineName(engineName)
+                    .engineVersion(1)
+                    .apiVersion(VK.getInstanceVersionSupported());
         }
     }
 
