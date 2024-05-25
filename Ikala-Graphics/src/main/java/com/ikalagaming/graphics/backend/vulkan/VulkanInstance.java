3c83b8314e361c1ca16355864656ec028bc32b5f
David Burks <chesburks@gmail.com>
2024-05-24 20:15:26 -0400

    Add more validation layer checks

--- Ikala-Graphics/src/main/java/com/ikalagaming/graphics/backend/vulkan/VulkanInstance.java
+++ Ikala-Graphics/src/main/java/com/ikalagaming/graphics/backend/vulkan/VulkanInstance.java
@@ -24,14 +24,18 @@ import org.lwjgl.vulkan.VkLayerProperties;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.LongBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class VulkanInstance implements Instance {
 
     private static final long NULL = 0L;
-    private static final String[] REQUIRED_LAYERS = {};
+    private static final String[] REQUIRED_EXTENSIONS = {};
 
     private static final String[] VALIDATION_LAYERS = {"VK_LAYER_KHRONOS_validation"};
+
+    private static final boolean ENABLE_VALIDATION = true;
 
     private final IntBuffer intOutput = MemoryUtil.memAllocInt(1);
     private final LongBuffer longOutput = MemoryUtil.memAllocLong(1);
@@ -54,6 +58,12 @@ public class VulkanInstance implements Instance {
         createVulkanInstance(window);
     }
 
+    /**
+     * Set up the vulkan instance.
+     *
+     * @param window The window we are setting up to render with.
+     * @throws RenderException If an unrecoverable issue occurs setting up vulkan.
+     */
     private void createVulkanInstance(@NonNull Window window) {
         try (MemoryStack stack = stackPush()) {
 
@@ -84,14 +94,83 @@ public class VulkanInstance implements Instance {
                 extensionNames.put(glfwExtensions.get(i));
             }
 
-            checkError(vkEnumerateInstanceExtensionProperties((String) null, intOutput, null));
+            vkEnumerateInstanceLayerProperties(intOutput, null);
+
+            if (intOutput.get(0) > 0) {
+                VkLayerProperties.Buffer availableLayers =
+                        VkLayerProperties.malloc(intOutput.get(0), stack);
+                checkError(vkEnumerateInstanceLayerProperties(intOutput, availableLayers));
+
+                for (String requiredLayer : REQUIRED_EXTENSIONS) {
+                    extensionNames.put(stack.ASCII(requiredLayer));
+                }
+
+                if (ENABLE_VALIDATION) {
+                    for (String validationLayer : VALIDATION_LAYERS) {
+                        extensionNames.put(stack.ASCII(validationLayer));
+                    }
+                }
+                extensionNames.flip();
+                checkLayers(availableLayers, extensionNames);
+            }
         }
     }
 
+    /**
+     * Check that the specified layers are available, and throw an exception if any are not.
+     *
+     * @param availableLayerNames The layer names that are available.
+     * @param requiredLayerNames The layers that we require.
+     * @throws RenderException If required layers are missing.
+     */
     private void checkLayers(
-            @NonNull MemoryStack stack,
-            @NonNull VkLayerProperties.Buffer available,
-            String... layers) {}
+            @NonNull VkLayerProperties.Buffer availableLayerNames,
+            PointerBuffer requiredLayerNames) {
+
+        List<String> missingLayers = new ArrayList<>();
+        for (int i = 0; i < requiredLayerNames.limit(); ++i) {
+            boolean found = false;
+
+            final String required = requiredLayerNames.getStringASCII(i);
+
+            for (int j = 0; j < availableLayerNames.capacity(); ++j) {
+                availableLayerNames.position(j);
+                if (required.equals(availableLayerNames.layerNameString())) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                missingLayers.add(required);
+            }
+        }
+
+        if (!missingLayers.isEmpty()) {
+            var layerNames = String.join(", ", missingLayers);
+            var message =
+                    SafeResourceLoader.getStringFormatted(
+                            "VULKAN_LAYERS_MISSING",
+                            GraphicsPlugin.getResourceBundle(),
+                            layerNames);
+            log.error(message);
+
+            List<String> layers = new ArrayList<>();
+            for (int j = 0; j < availableLayerNames.capacity(); ++j) {
+                var layer = availableLayerNames.get(j);
+                layers.add(
+                        String.format(
+                                "%s (%s)", layer.layerNameString(), layer.descriptionString()));
+            }
+            log.info(
+                    SafeResourceLoader.getStringFormatted(
+                            "VULKAN_LAYERS_LIST",
+                            GraphicsPlugin.getResourceBundle(),
+                            String.join(", ", layers)));
+
+            throw new RenderException(message);
+        }
+    }
 
     @Override
     public void cleanup() {}
