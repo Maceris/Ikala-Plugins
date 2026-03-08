41e8493c23955b05f09a255ff3225fbca714110c
David Burks <chesburks@gmail.com>
2026-03-08 17:59:26 -0400

    Start designing font atlas

--- Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/Font.java
+++ Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/Font.java
@@ -2,4 +2,15 @@ package com.ikalagaming.graphics.frontend.gui.data;
 
 public class Font {
     // TODO(ches) font
+
+    /**
+     * Convert the height of a letter from pt (points) to px (pixels).
+     *
+     * @param pt The size in points.
+     * @return The height in pixels.
+     */
+    public static int estimatedHeight(int pt) {
+        float pixels = pt * 1.333f;
+        return (int) pixels;
+    }
 }
--- Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/FontAtlas.java
+++ Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/FontAtlas.java
@@ -1,5 +1,54 @@
 package com.ikalagaming.graphics.frontend.gui.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FontAtlas {
     // TODO(ches) font
+
+    /** Maximum height of a font atlas image, in pixels. */
+    public static final int MAX_FONT_ATLAS_IMAGE_HEIGHT = 1024;
+
+    /** Maximum width of a font atlas image, in pixels. */
+    public static final int MAX_FONT_ATLAS_IMAGE_WIDTH = 1024;
+
+    public static final int INVALID_INDEX = -1;
+    private final List<FontPage> pages;
+
+    public static int charactersPerPage(int size) {
+        int squarePixels = MAX_FONT_ATLAS_IMAGE_HEIGHT * MAX_FONT_ATLAS_IMAGE_WIDTH;
+        int roughEstimate = Font.estimatedHeight(size);
+        roughEstimate *= roughEstimate;
+
+        return squarePixels / roughEstimate;
+    }
+
+    public FontAtlas() {
+        pages = new ArrayList<>();
+    }
+
+    public int getFontMapIndex(char c, int size) {
+        return INVALID_INDEX;
+    }
+
+    public void createDefaultPage(int fontSize) {
+        boolean ranOutOfSpace;
+        char nextCharacter = ' ';
+
+        do {
+            FontPage page = new FontPage(fontSize);
+            pages.add(page);
+            ranOutOfSpace = false;
+
+            // Add all the printable ascii characters
+            for (char c = nextCharacter; c <= '~'; ++c) {
+                if (!page.addCharacter(c)) {
+                    ranOutOfSpace = true;
+                    nextCharacter = c;
+                    break;
+                }
+            }
+        } while (!ranOutOfSpace);
+        // TODO(ches) add extended latin characters, bake
+    }
 }
