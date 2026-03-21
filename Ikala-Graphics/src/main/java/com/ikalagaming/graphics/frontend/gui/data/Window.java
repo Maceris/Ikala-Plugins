0b2106ade3063914b894a660d67b7b36ebd86aad
David Burks <chesburks@gmail.com>
2026-03-20 22:18:26 -0400

    Sort fields of window

--- Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/Window.java
+++ Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/Window.java
@@ -14,55 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Window {
-    public final String name;
-    public int id;
-    public int flags;
-    public int flagsAsChildWindow;
-    public final List<Window> childWindows;
-    public Viewport viewport;
-    public final Vector2i position;
-
-    /**
-     * Only non-null if this window has a close button, used to communicate elsewhere that we closed
-     * the window.
-     */
-    public IkBoolean open;
-
-    /** Size, which might be smaller than the full size if minimized. */
-    public final Vector2i sizeCurrent;
-
-    /** The full size of the window when not collapsed. */
-    public final Vector2i sizeFull;
-
-    /** Size the window would like to be, given its contents. */
-    public final Vector2i sizeDesired;
-
-    /** Size that was requested for the window before it was drawn. */
-    public final Vector2i sizeRequested;
-
-    public final Vector2i padding;
-    public float rounding;
-    public int borderSize;
-    public int titleBarHeight;
-    public int menuBarHeight;
-
-    public int idWithinParent;
-    public int idAsPopupWindow;
-    public final Vector2i scrollPosition;
-    public final Vector2i scrollExtent;
-    public final Vector2i scrollTarget;
-
-    /** X is the width of the vertical scrollbar, y is the height of the horizontal scrollbar. */
-    public final Vector2i scrollbarSizes;
-
-    public boolean showScrollbarX;
-    public boolean showScrollbarY;
     public boolean active;
-    public boolean collapsed;
-    public boolean collapseToggleRequested;
-    public boolean hidden;
-    public boolean skipRenderingContents;
-    public boolean reuseLastFrameContents;
 
     /**
      * Whether the window is in the process of appearing after being hidden or inactive, or the
@@ -70,10 +22,75 @@ public class Window {
      */
     public boolean appearing;
 
-    public Direction borderBeingHovered;
+    public int baseOffsetCurrentLine;
+    public int baseOffsetPreviousLine;
     public Direction borderBeingDragged;
+    public Direction borderBeingHovered;
+    public int borderSize;
 
+    public final List<Window> childWindows;
+
+    public boolean collapsed;
+
+    public boolean collapseToggleRequested;
+
+    public float currentItemWidth;
+
+    public int currentTableIndex;
+
+    public float currentTextWrapPosition;
+
+    /**
+     * Grows as content is added, and used to determine the content region next frame, for
+     * auto-resize.
+     */
+    public final Vector2i cursorIdealMaxPosition;
+
+    /**
+     * Grows as content is added, and used to determine the content region next frame, for scrolling
+     * and auto-resize.
+     */
+    public final Vector2i cursorMaxPosition;
+
+    public final Vector2i cursorPosition;
+    public final Vector2i cursorPreviousLinePosition;
+
+    public final Vector2i cursorStartPosition;
+    public DrawList drawList;
+    public int flags;
+    public int flagsAsChildWindow;
+    public boolean hidden;
+
+    public int id;
+
+    public int idAsPopupWindow;
     public IntArrayList idStack;
+    public int idWithinParent;
+    public float indent;
+    public final FloatArrayList itemWidthStack;
+    public final Vector2i lineSizeCurrent;
+    public final Vector2i lineSizePrevious;
+    public int menuBarHeight;
+
+    public final String name;
+
+    /**
+     * Only non-null if this window has a close button, used to communicate elsewhere that we closed
+     * the window.
+     */
+    public IkBoolean open;
+
+    public final Vector2i padding;
+
+    public Window parentWindow;
+
+    public final Vector2i position;
+
+    /** The region that contains the contents, including parts scrolled out of view. */
+    public final RectFloat rectContent;
+
+    /** Current clipping rect, since we can push and pop clip rects. */
+    public final RectFloat rectCurrentClip;
 
     /** The outer region of the window. */
     public final RectFloat rectFull;
@@ -84,130 +101,119 @@ public class Window {
     /** Inner rect, but shrunk by 0.5 * padding, and clipped by the viewport or parent clip rect. */
     public final RectFloat rectInnerClip;
 
-    /** The region that contains the contents, including parts scrolled out of view. */
-    public final RectFloat rectContent;
-
-    /** Current clipping rect, since we can push and pop clip rects. */
-    public final RectFloat rectCurrentClip;
-
-    public DrawList drawList;
-    public Window parentWindow;
+    public boolean reuseLastFrameContents;
     public Window rootWindow;
-    public Window rootWindowIncludingPopups;
-    public Window rootWindowForTitleBarHighlight;
     public Window rootWindowForNavigation;
-
-    public final Vector2i cursorPosition;
-    public final Vector2i cursorStartPosition;
-    public final Vector2i cursorPreviousLinePosition;
+    public Window rootWindowForTitleBarHighlight;
+    public Window rootWindowIncludingPopups;
 
     /**
-     * Grows as content is added, and used to determine the content region next frame, for scrolling
-     * and auto-resize.
+     * Corner rounding radius for the window. Window flags are used to specify which corners are
+     * rounded.
      */
-    public final Vector2i cursorMaxPosition;
+    public float rounding;
 
-    /**
-     * Grows as content is added, and used to determine the content region next frame, for
-     * auto-resize.
-     */
-    public final Vector2i cursorIdealMaxPosition;
-
-    public final Vector2i lineSizePrevious;
-    public final Vector2i lineSizeCurrent;
-    public int baseOffsetCurrentLine;
-    public int baseOffsetPreviousLine;
     public boolean sameLine;
-    public boolean setPos;
-    public float indent;
-    public int treeDepth;
 
-    public int currentTableIndex;
-    public float currentItemWidth;
-    public float currentTextWrapPosition;
-    public final FloatArrayList itemWidthStack;
+    /** X is the width of the vertical scrollbar, y is the height of the horizontal scrollbar. */
+    public final Vector2i scrollbarSizes;
+
+    public final Vector2i scrollExtent;
+
+    public final Vector2i scrollPosition;
+
+    public final Vector2i scrollTarget;
+    public boolean setPos;
+    public boolean showScrollbarX;
+    public boolean showScrollbarY;
+
+    /** Size, which might be smaller than the full size if minimized. */
+    public final Vector2i sizeCurrent;
+
+    /** Size the window would like to be, given its contents. */
+    public final Vector2i sizeDesired;
+
+    /** The full size of the window when not collapsed. */
+    public final Vector2i sizeFull;
+
+    /** Size that was requested for the window before it was drawn. */
+    public final Vector2i sizeRequested;
+
+    public boolean skipRenderingContents;
     public final FloatArrayList textWrapPositionStack;
+    public int titleBarHeight;
+    public int treeDepth;
+    public Viewport viewport;
 
     public Window(@NonNull String name) {
-        this.name = name;
-        id = 0;
-        flags = WindowFlags.NONE;
-        flagsAsChildWindow = WindowFlags.NONE;
-        childWindows = new ArrayList<>();
-        viewport = null;
-        position = new Vector2i(0, 0);
-        open = new IkBoolean();
-        sizeCurrent = new Vector2i(0, 0);
-        sizeFull = new Vector2i(0, 0);
-        sizeDesired = new Vector2i(0, 0);
-        sizeRequested = new Vector2i(0, 0);
-        padding = new Vector2i(0, 0);
-        rounding = 0.0f;
-        borderSize = 0;
-        titleBarHeight = 0;
-        menuBarHeight = 0;
-        idWithinParent = 0;
-        idAsPopupWindow = 0;
-        scrollPosition = new Vector2i(0, 0);
-        scrollExtent = new Vector2i(0, 0);
-        scrollTarget = new Vector2i(0, 0);
-        scrollbarSizes = new Vector2i(0, 0);
-        showScrollbarX = false;
-        showScrollbarY = false;
         active = false;
+        appearing = false;
+        baseOffsetCurrentLine = 0;
+        baseOffsetPreviousLine = 0;
+        borderBeingDragged = Direction.NONE;
+        borderBeingHovered = Direction.NONE;
+        borderSize = 0;
+        childWindows = new ArrayList<>();
         collapsed = false;
         collapseToggleRequested = false;
+        currentItemWidth = 0.0f;
+        currentTableIndex = 0;
+        currentTextWrapPosition = 0.0f;
+        cursorIdealMaxPosition = new Vector2i(0, 0);
+        cursorMaxPosition = new Vector2i(0, 0);
+        cursorPosition = new Vector2i(0, 0);
+        cursorPreviousLinePosition = new Vector2i(0, 0);
+        cursorStartPosition = new Vector2i(0, 0);
+        drawList = new DrawList(name);
+        flags = WindowFlags.NONE;
+        flagsAsChildWindow = WindowFlags.NONE;
         hidden = false;
-        skipRenderingContents = false;
-        reuseLastFrameContents = false;
-        appearing = false;
-        borderBeingHovered = Direction.NONE;
-        borderBeingDragged = Direction.NONE;
+        id = 0;
+        idAsPopupWindow = 0;
         idStack = new IntArrayList();
+        idWithinParent = 0;
+        indent = 0.0f;
+        itemWidthStack = new FloatArrayList();
+        lineSizeCurrent = new Vector2i(0, 0);
+        lineSizePrevious = new Vector2i(0, 0);
+        menuBarHeight = 0;
+        this.name = name;
+        open = new IkBoolean();
+        padding = new Vector2i(0, 0);
+        parentWindow = null;
+        position = new Vector2i(0, 0);
+        rectContent = new RectFloat(0, 0, 0, 0);
+        rectCurrentClip = new RectFloat(0, 0, 0, 0);
         rectFull = new RectFloat(0, 0, 0, 0);
         rectInner = new RectFloat(0, 0, 0, 0);
         rectInnerClip = new RectFloat(0, 0, 0, 0);
-        rectContent = new RectFloat(0, 0, 0, 0);
-        rectCurrentClip = new RectFloat(0, 0, 0, 0);
-        drawList = new DrawList(name);
-        parentWindow = null;
+        reuseLastFrameContents = false;
         rootWindow = null;
-        rootWindowIncludingPopups = null;
-        rootWindowForTitleBarHighlight = null;
         rootWindowForNavigation = null;
-        cursorPosition = new Vector2i(0, 0);
-        cursorStartPosition = new Vector2i(0, 0);
-        cursorPreviousLinePosition = new Vector2i(0, 0);
-        cursorMaxPosition = new Vector2i(0, 0);
-        cursorIdealMaxPosition = new Vector2i(0, 0);
-        lineSizePrevious = new Vector2i(0, 0);
-        lineSizeCurrent = new Vector2i(0, 0);
-        baseOffsetCurrentLine = 0;
-        baseOffsetPreviousLine = 0;
+        rootWindowForTitleBarHighlight = null;
+        rootWindowIncludingPopups = null;
+        rounding = 0.0f;
         sameLine = false;
+        scrollbarSizes = new Vector2i(0, 0);
+        scrollExtent = new Vector2i(0, 0);
+        scrollPosition = new Vector2i(0, 0);
+        scrollTarget = new Vector2i(0, 0);
         setPos = false;
-        indent = 0.0f;
-        treeDepth = 0;
-        currentTableIndex = 0;
-        currentItemWidth = 0.0f;
-        currentTextWrapPosition = 0.0f;
-        itemWidthStack = new FloatArrayList();
+        showScrollbarX = false;
+        showScrollbarY = false;
+        sizeCurrent = new Vector2i(0, 0);
+        sizeDesired = new Vector2i(0, 0);
+        sizeFull = new Vector2i(0, 0);
+        sizeRequested = new Vector2i(0, 0);
+        skipRenderingContents = false;
         textWrapPositionStack = new FloatArrayList();
+        titleBarHeight = 0;
+        treeDepth = 0;
+        viewport = null;
     }
 
     public boolean hasCloseButton() {
         return open != null;
-    }
-
-    public void updateCursorBeforeDrawing() {
-        if (!sameLine) {
-            lineSizePrevious.set(lineSizeCurrent);
-            lineSizeCurrent.set(0, 0);
-            cursorPreviousLinePosition.set(cursorPosition);
-            cursorPosition.set(cursorStartPosition.x, cursorPosition.y);
-            cursorPosition.add(
-                    0, lineSizePrevious.y + IkGui.getContext().style.variable.itemSpacing.y);
-        }
     }
 
     public void updateCursorAfterDrawing(int lastItemWidth, int lastItemHeight) {
@@ -219,5 +225,16 @@ public class Window {
         }
         cursorPosition.add(lastItemWidth, 0);
         sameLine = false;
+    }
+
+    public void updateCursorBeforeDrawing() {
+        if (!sameLine) {
+            lineSizePrevious.set(lineSizeCurrent);
+            lineSizeCurrent.set(0, 0);
+            cursorPreviousLinePosition.set(cursorPosition);
+            cursorPosition.set(cursorStartPosition.x, cursorPosition.y);
+            cursorPosition.add(
+                    0, lineSizePrevious.y + IkGui.getContext().style.variable.itemSpacing.y);
+        }
     }
 }
