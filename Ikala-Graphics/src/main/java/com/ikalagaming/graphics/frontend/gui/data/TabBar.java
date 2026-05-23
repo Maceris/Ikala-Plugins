f3b9dc244704148ec861d26332b5e1fb4792f157
David Burks <chesburks@gmail.com>
2026-05-22 21:04:56 -0400

    Make fields visible, add constructor

--- Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/TabBar.java
+++ Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/TabBar.java
@@ -1,49 +1,88 @@
 package com.ikalagaming.graphics.frontend.gui.data;
 
+import com.ikalagaming.graphics.frontend.gui.flags.TabBarFlags;
 import com.ikalagaming.graphics.frontend.gui.util.RectFloat;
 
 import org.joml.Vector2f;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TabBar {
-    private Window window;
-    private ArrayList<TabItem> tabs;
+    public Window window;
+    public List<TabItem> tabs;
 
     /**
      * @see com.ikalagaming.graphics.frontend.gui.flags.TabBarFlags
      */
-    private int flags;
+    public int flags;
 
-    private int ID;
-    private int selectedTabID;
-    private int nextSelectedTabID;
-    private int visibleTabID;
-    private int currentFrameVisible;
-    private int previousFrameVisible;
-    private RectFloat bounds;
-    private float currentTabContentHeight;
-    private float previousTabContentHeight;
-    private float widthAllTabs;
-    private float widthAllTabsIdeal;
-    private float scrollingAnimation;
-    private float scrollingTarget;
-    private float scrollingTargetDistanceUntilVisible;
-    private float scrollingSpeed;
-    private float scrollingRectMinX;
-    private float scrollingRectMaxX;
-    private float separatorMinX;
-    private float separatorMaxX;
-    private int reorderRequestTabID;
-    private IkShort reorderRequestOffset;
-    private IkByte beginCount;
-    private boolean layoutRequested;
-    private boolean visibleTabSubmitted;
-    private boolean tabHadNewItemAdded;
-    private IkShort tabsSubmittedThisFrame;
-    private IkShort lastTabIndex;
-    private float itemSpacingY;
-    private Vector2f framePadding;
-    private Vector2f backupCursorPosition;
-    private StringBuilder tabNames;
+    public int id;
+    public int selectedTabID;
+    public int nextSelectedTabID;
+    public int visibleTabID;
+    public int currentFrameVisible;
+    public int previousFrameVisible;
+    public RectFloat bounds;
+    public float currentTabContentHeight;
+    public float previousTabContentHeight;
+    public float widthAllTabs;
+    public float widthAllTabsIdeal;
+    public float scrollingAnimation;
+    public float scrollingTarget;
+    public float scrollingTargetDistanceUntilVisible;
+    public float scrollingSpeed;
+    public float scrollingRectMinX;
+    public float scrollingRectMaxX;
+    public float separatorMinX;
+    public float separatorMaxX;
+    public int reorderRequestTabID;
+    public IkShort reorderRequestOffset;
+    public IkByte beginCount;
+    public boolean layoutRequested;
+    public boolean visibleTabSubmitted;
+    public boolean tabHadNewItemAdded;
+    public IkShort tabsSubmittedThisFrame;
+    public IkShort lastTabIndex;
+    public float itemSpacingY;
+    public Vector2f framePadding;
+    public Vector2f backupCursorPosition;
+    public StringBuilder tabNames;
+
+    public TabBar() {
+        window = null;
+        tabs = new ArrayList<>();
+        flags = TabBarFlags.NONE;
+        id = 0;
+        selectedTabID = 0;
+        nextSelectedTabID = 0;
+        visibleTabID = 0;
+        currentFrameVisible = 0;
+        previousFrameVisible = 0;
+        bounds = new RectFloat(0.0f, 0.0f, 0.0f, 0.0f);
+        currentTabContentHeight = 0.0f;
+        previousTabContentHeight = 0.0f;
+        widthAllTabs = 0.0f;
+        widthAllTabsIdeal = 0.0f;
+        scrollingAnimation = 0.0f;
+        scrollingTarget = 0.0f;
+        scrollingTargetDistanceUntilVisible = 0.0f;
+        scrollingSpeed = 0.0f;
+        scrollingRectMinX = 0.0f;
+        scrollingRectMaxX = 0.0f;
+        separatorMinX = 0.0f;
+        separatorMaxX = 0.0f;
+        reorderRequestTabID = 0;
+        reorderRequestOffset = new IkShort();
+        beginCount = new IkByte();
+        layoutRequested = false;
+        visibleTabSubmitted = false;
+        tabHadNewItemAdded = false;
+        tabsSubmittedThisFrame = new IkShort();
+        lastTabIndex = new IkShort();
+        itemSpacingY = 0.0f;
+        framePadding = new Vector2f(0.0f, 0.0f);
+        backupCursorPosition = new Vector2f(0.0f, 0.0f);
+        tabNames = new StringBuilder();
+    }
 }
--- Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/TabItem.java
+++ Ikala-Graphics/src/main/java/com/ikalagaming/graphics/frontend/gui/data/TabItem.java
@@ -1,22 +1,39 @@
 package com.ikalagaming.graphics.frontend.gui.data;
 
+import com.ikalagaming.graphics.frontend.gui.flags.TabItemFlags;
+
 public class TabItem {
 
-    private int ID;
+    public int id;
 
     /**
      * @see com.ikalagaming.graphics.frontend.gui.flags.TabItemFlags
      */
-    private int tabItemFlags;
+    public int tabItemFlags;
 
-    private int lastFrameVisible;
-    private int lastFrameSelected;
-    private float offset;
-    private float width;
-    private float labelWidth;
-    private float requestedWidth;
-    private IkInt nameOffset;
-    private IkShort beginOrder;
-    private IkShort indexDuringLayout;
-    private boolean closeRequested;
+    public int lastFrameVisible;
+    public int lastFrameSelected;
+    public float offset;
+    public float width;
+    public float labelWidth;
+    public float requestedWidth;
+    public IkInt nameOffset;
+    public IkShort beginOrder;
+    public IkShort indexDuringLayout;
+    public boolean closeRequested;
+
+    public TabItem() {
+        id = 0;
+        tabItemFlags = TabItemFlags.NONE;
+        lastFrameVisible = 0;
+        lastFrameSelected = 0;
+        offset = 0.0f;
+        width = 0.0f;
+        labelWidth = 0.0f;
+        requestedWidth = 0.0f;
+        nameOffset = new IkInt();
+        beginOrder = new IkShort();
+        indexDuringLayout = new IkShort();
+        closeRequested = false;
+    }
 }
